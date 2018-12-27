d414c46e4869d1b345e723c4eb37b1e7436edc83
LouisDelon <louleman34@gmail.com>
2018-12-27 17:08:20 +0100

    create user controller and update user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
   def show
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar, :username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-
-  get 'users/update'
-
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, only: [:show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
