61b527ba6c8488513b924e8f772b90719a48d447
LouisDelon <louleman34@gmail.com>
2018-12-27 12:50:46 +0100

    set devise gem sanitizer and autofill username when create an account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  before_create :set_username
+
   mount_uploader :avatar, PhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def set_username
+      self.username = "#{self.email[/^[^@]+/]}-#{SecureRandom.hex(1)}"
+    end
 
 end
