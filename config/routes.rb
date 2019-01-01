ba6ee09c30ea749dfd8bd7d65aa4fd1603756450
LouisDelon <louleman34@gmail.com>
2019-01-01 21:42:57 +0100

    add routes for movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'movies/index'
-
-  get 'movies/create'
-
-  get 'movies/destroy'
-
-  get 'movies/edit'
-
-  get 'movies/update'
-
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:show, :update]
+  resources :users, only: [:show, :update] do
+    resources :movies
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
