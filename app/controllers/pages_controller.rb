5eda1f7237f03653cb69136f6190a669f7d25e82
LouisDelon <louleman34@gmail.com>
2019-01-05 13:24:06 +0100

    seed the db with api call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,30 +6,4 @@ class PagesController < ApplicationController
     @movies = Movie.all
   end
 
-  private
-
-  def parsing(url)
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    JSON.parse(response)
-  end
-
-  def instantiate_movies
-    build_api_url
-    movies_list = parsing(movies_url)
-    movies_list["results"].each do |movie|
-      Movie.create!(
-        title: movie["title"],
-        resume: movie["overview"],
-        photo: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
-        user_id: 18
-        )
-    end
-  end
-
-  def build_api_url
-    api_key = ENV['SECRET_KEY_THE_MOVIE_DB']
-    movies_url = "https://api.themoviedb.org/4/list/1?page=2&api_key=#{api_key}&sort_by=original_order.asc"
-  end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def parsing(url)
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+end
+
+Movie.destroy_all
+
+puts "make api call"
+
+api_key = ENV['SECRET_KEY_THE_MOVIE_DB']
+movies_url = "https://api.themoviedb.org/4/list/1?page=2&api_key=#{api_key}&sort_by=original_order.asc"
+movies_list = parsing(movies_url)
+
+puts "create Movies form api call"
+
+movies_list["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    resume: movie["overview"],
+    photo: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    user_id: 18
+    )
+end
+
+
