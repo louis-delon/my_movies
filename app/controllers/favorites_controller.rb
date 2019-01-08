8522ce9b31fca6bb8d6bf3e1d32f49757469ad87
LouisDelon <louleman34@gmail.com>
2019-01-08 14:17:15 +0100

    display favorites movies for a user

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 class FavoritesController < ApplicationController
 
   def index
+    @favorites = Favorite.where(user_id: current_user.id )
+    @movies = @favorites.map { |favorite| Movie.where(id: favorite.movie_id)}
   end
 
   def create
