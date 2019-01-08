ca799cf0f5c362063828178eb20a36f92bb9d2a8
LouisDelon <louleman34@gmail.com>
2019-01-08 13:22:20 +0100

    add favorite route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'favorites/index'
-
   devise_for :users
   root to: 'pages#home'
 
   resources :users, only: [:show, :update] do
-    # resources :movies, only: [:index]
+    resources :favorites, only: [:index, :create]
   end
   resources :movies
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
