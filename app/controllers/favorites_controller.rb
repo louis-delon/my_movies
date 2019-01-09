7197df94310ea2698427d34d576f6c3d33a6043a
LouisDelon <louleman34@gmail.com>
2019-01-09 09:22:11 +0100

    truncate resume on movie cards

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,13 +6,15 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @favorite = Favorite.new
     @favorite.user = current_user
+    @movie = Movie.find(params[:movie_id])
     @favorite.movie = @movie
     if @favorite.save!
-      puts "xxxxxxxxx JE SUIS DANS CREATE FAVORITE xxxxxxxxxxx"
       redirect_back(fallback_location: root_path)
     end
   end
 end
+
+
+# si j'ai déjà ajouté
